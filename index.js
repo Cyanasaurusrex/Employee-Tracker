c6866c02ac49332d04544da7bfcfa14c147f1528
Nathan Moon <moonman700@gmail.com>
2023-04-17 17:30:13 -0400

    added switch statement for main menu and changed db queries to promises

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const inquirer = require('inquirer');
+const cTable = require('console.table')
+const mysql = require('mysql2');
+
 const questions = [   
     {
         type: 'list',
         message: 'WHat would you like to do?',
         choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role'
+            
         ],
         name: 'selection'
     }
 ]
-const cTable = require('console.table')
-const mysql = require('mysql2');
+
+
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -27,32 +24,106 @@ const db = mysql.createConnection(
     console.log('Connected to the employee_chart_db database\n\n')
 )
 
-db.query(`
-    SELECT e.id,e.first_name, e.last_name, r.title, d.name, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
-    FROM employees e
-    JOIN roles r ON e.role_id = r.id
-    JOIN departments d on d.id = r.department_id
-    LEFT JOIN employees m ON e.manager_id = m.id;`,
-    function (err, results) {
-        console.table(results);
-        
-});
 
-db.query(`
-    SELECT *
-    FROM employee_chart_db.departments
-    ORDER BY name ASC;`,
-    function (err, results) {
-        console.table(results);
-        
-});
+async function mainMenu() {
+    let choice = '';
+    while (choice !== 'Exit') {
+      const answer = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'menuChoice',
+          message: 'What would you like to do?',
+          choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit'
+          ]
+        }
+      ]);
+      choice = answer.menuChoice;
+      switch (choice) {
 
-db.query(`
-SELECT * FROM employee_chart_db.roles`,
-    function (err, results) {
-        console.table(results);
-        
-});
 
-inquirer
-.prompt(questions)
+        // view all departments
+        case 'View all departments':
+            try {
+                const [rows, fields] = await db.promise().query(`
+                SELECT *
+                FROM employee_chart_db.departments
+                ORDER BY name ASC;
+                `);
+                console.table(rows);        
+            } catch (err) {
+                console.error(err);
+            }
+            break;
+
+
+        // View all roles
+        case 'View all roles':
+            try {
+                const [rows, fields] = await db.promise().query(`
+                    SELECT * FROM employee_chart_db.roles`);
+                    console.table(rows)
+            } catch (err) {
+                console.error(err)
+            }
+            break;
+
+
+        // View all employees
+        case 'View all employees':
+            try {
+                const [rows, fields] = await db.promise().query(`
+                  SELECT e.id,e.first_name, e.last_name, r.title, d.name, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+                  FROM employees e
+                  JOIN roles r ON e.role_id = r.id
+                  JOIN departments d on d.id = r.department_id
+                  LEFT JOIN employees m ON e.manager_id = m.id;
+                `);
+                console.table(rows);
+            } catch (err) {
+                console.error(err);
+            }
+            break;
+
+
+        // Add a department
+        case 'Add a department':
+            console.log('no rly3')
+          break;
+
+
+        // Add a role  
+        case 'Add a role':
+            console.log('no rly4')
+          break;
+
+
+        // Add an emmployee  
+        case 'Add an employee':
+            console.log('no rly5')
+          break;
+
+
+        // Update an employee role  
+        case 'Update an employee role':
+            console.log('no rly6')
+          break;
+
+
+        // Exits the main menu loop  
+        case 'Exit':
+          db.end()
+          break;
+      }
+    }
+  }
+  
+  // call the mainMenu function to start the menu loop
+  mainMenu();
